871ea2560046408b4ab29104949b7b07e9f12423
1slom-media <islombektagayev1@gmail.com>
2025-04-24 12:16:04 +0500

    ad controller

--- src/smart-bill/smart-bill.controller.ts
+++ src/smart-bill/smart-bill.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { SmartBillService } from './smart-bill.service';
+import {
+  CheckLimitDto,
+  CreateOrderDto,
+  UpdateOrderStatusDto,
+} from './dto/smart.dto';
 
+@ApiTags('Order')
 @Controller('smart-bill')
-export class SmartBillController {}
+export class SmartBillController {
+  constructor(public smartBill: SmartBillService) {}
+
+  @ApiOperation({ summary: 'CheckLimit' })
+  @Post('/check-limit')
+  async checkLimit(@Body() data: CheckLimitDto) {
+    return this.smartBill.checkLimit(data);
+  }
+
+  @ApiOperation({ summary: 'Create order' })
+  @Post('/create-order')
+  async createOrder(@Body() data: CreateOrderDto) {
+    return this.smartBill.createOrder(data);
+  }
+
+  @ApiOperation({ summary: 'Update order' })
+  @Put('/update-order/:id')
+  async updateOrder(
+    @Body() data: UpdateOrderStatusDto,
+    @Param('id') id: string,
+  ) {
+    return this.smartBill.updateOrderStatus(id, data);
+  }
+
+  @ApiOperation({ summary: 'Get order' })
+  @Put('/get-order/:id')
+  async getOrder(@Param('id') id: string) {
+    return this.smartBill.getOrderById(id);
+  }
+}
